4db26b0bd39efc33aee397f9efaacfc91d0e1110
Meghanna magesh <129411055+09Meg@users.noreply.github.com>
2023-04-12 17:08:37 +0100

    Update airline.py

--- airline.py
+++ airline.py
@@ -3,10 +3,10 @@ from tkinter import *
 from tkinter.ttk import Combobox
 from tkinter import messagebox
 # Connect to MySQL database
-conn = mysql.connector.connect
-    host="localhost"
+conn = mysql.connector.connect(
+    host="localhost",
     user="root",
     password="sunshine",
-    database='smj_airline_reservation"
+    database="smj_airline_reservation"
 )
 cursor = conn.cursor()
